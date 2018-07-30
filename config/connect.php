73a078aa891df9ce6d76e528befa201b3fc35653
jeniaadellia99 <adellia.jenia@gmail.com>
2018-07-30 14:13:37 +0700

    project yii2

--- config/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "perpustakaan";
-	$koneksi = mysqli_connect($server,$username,$password, $database) or die("Koneksi gagal");
-?>
